5abfb4fc913df689fb6eb1d5f880e769f8b84469
Himansu Sekhar Lenka <himansu.lenka173@gmail.com>
2024-07-22 12:01:24 +0530

    new featesa added

--- src/Components/Homepage/MyFeedHomepage.jsx
+++ src/Components/Homepage/MyFeedHomepage.jsx
@@ -42,7 +42,7 @@ const MyFeedHomepage = () => {
           MEN
         </Text>
       </Stack>
-      <Stack
+      {focusMen && <Stack
         pl={"20px"}
         direction={"row"}
         w={useBreakpointValue({ base: "99%", lg: "60%" })}
@@ -188,7 +188,166 @@ const MyFeedHomepage = () => {
             </Text>
           </Stack>
         </Stack>
-      </Stack>
+      </Stack>}
+
+      {focusWomen && <Stack
+        pl={"20px"}
+        direction={"row"}
+        w={useBreakpointValue({ base: "99%", lg: "60%" })}
+        m={"auto"}
+        color={"black"}
+      >
+        <Stack align={"center"}>
+          <Avatar
+            border={"2px solid #c4dd91"}
+            p={"2px"}
+            size="lg"
+            name="MY"
+            src="https://img1.junaroad.com//assets/images/mobileNotif/img-1628498908811.jpg"
+          />
+          <Text fontWeight={"500"} fontSize={"10px"}>
+            MY FEED
+          </Text>
+        </Stack>
+        <div
+          style={{
+            backgroundColor: "black",
+            width: "1px",
+            height: "40px",
+            opacity: "20%",
+            marginTop: "30px",
+            marginRight: "15px",
+            marginLeft: "15px",
+          }}
+        ></div>
+        <Stack
+          overflow={useBreakpointValue({ base: "scroll", lg: "" })}
+          gap={"19px"}
+          align={"flex-end"}
+          direction={"row"}
+        >
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/kurtas.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              KURTAS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/tops.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              TOPS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/dresses.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              DRESSES
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/sarees.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              SAREES
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="https://img0.junaroad.com/images/icons/women/suits.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              SUITS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="https://img0.junaroad.com/images/icons/women/bottoms.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              BOTTOMS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/bags.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              BAGS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="	https://img0.junaroad.com/images/icons/women/footwear.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              FOOTWEAR
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="https://img0.junaroad.com/images/icons/women/kids.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              KIDS
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="https://img0.junaroad.com/images/icons/women/home.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              HOME
+            </Text>
+          </Stack>
+
+          <Stack _hover={{ cursor: "pointer" }} align={"center"}>
+            <Avatar
+              size="md"
+              name="Ryan Florence"
+              src="https://img0.junaroad.com/images/icons/women/winter.png"
+            />
+            <Text fontWeight={"500"} fontSize={"9.5px"}>
+              WINTER
+            </Text>
+          </Stack>
+        </Stack>
+      </Stack>}
     </>
   );
 };
